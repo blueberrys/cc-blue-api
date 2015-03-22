d16a1524e27981743f27ce4cc2e1a60ff65df4c5
Blueberry <potatebleh+git@gmail.com>
2015-03-22 17:24:22 -0400

    fix loading 6

--- BlueAPI.lua
+++ BlueAPI.lua
@@ -8,13 +8,20 @@ local root = string.match(shell.getRunningProgram(), "(.-)([^//]-([^%.]+))$")
 
 function load(module)
 	_G[module] = nil
-	return os.loadAPI(root .. module)
+
+	local path = "root" .. module
+	local success = os.loadAPI(path)
+	if not success then
+		path = "root" .. module .. ".lua"
+		success = os.loadAPI(path)
+	end
+
+	return success
 end
 
--- load("b_git")
+load("b_git")
 -- b_git.install()
 
 load("b_io")
-print("ok? ", b_io)
 b_io.pagedPrint("OK!")
 
