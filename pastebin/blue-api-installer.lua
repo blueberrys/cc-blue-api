42b99ec737945c6d4b4d11e716cc1a45a21fff59
Blueberry <potatebleh+git@gmail.com>
2015-03-24 13:11:11 -0400

    added pastebin
    a lot of work in ensureBlueApi, it's much more useful now and flows
    better with BlueAPI installer

---
+++ pastebin/blue-api-installer.lua
@@ -0,0 +1,110 @@
+--[[
+BlueAPI Installer
+Version 1.1
+
+Installs BlueAPI
+]]
+
+--[[
+Usage:
+
+pastebin run yy7gqfBQ [files..]
+
+Files should be relative paths of additional files to download
+If blank, all files will be downloaded
+Custom installation is slower because it uses synchronous http
+Only use it if you need a few files
+
+]]
+
+local user = "blueberrys"
+local repo = "cc-blue-api"
+
+local minFiles = {
+	"init.lua",
+	"version",
+	"apis/b_api.lua",
+	"apis/b_files.lua",
+	"apis/b_git.lua",
+	"apis/b_http.lua",
+	"apis/b_io.lua",
+	"apis/b_startup.lua",
+	"apis/b_update.lua",
+}
+local exclFiles = {
+	"README.md",
+	"pastebin",
+}
+
+local dir = "blue-api"
+local run = fs.combine(dir, "init.lua")
+-- local alias = "blu"
+
+local custom
+local params = {...}
+for _, a in pairs(params) do
+	if not custom then
+		custom = {}
+	end
+	table.insert(custom, a)
+end
+
+--
+
+local function install(username, repo, branch, files, path)
+	branch = branch or "master"
+	path = path or ""
+
+	print("Downloading files from github")
+	for _, file in pairs(files) do
+		print("Downloading " .. file)
+		-- https://raw.githubusercontent.com/blueberrys/cc-blue-api/master/init.lua
+		local url = "https://raw.githubusercontent.com/" .. username .. "/" .. repo .. "/" .. branch .. "/" .. file
+		local resp = http.get(url)
+		local data = resp and resp.readAll()
+		
+		if data and data ~= "Not Found" then
+			local f = fs.open(fs.combine(path, file), "w")
+			f.write(data)
+			f.close()
+		end
+	end
+end
+
+local function addFiles(filesToAdd)
+	for _, addFile in pairs(filesToAdd) do
+		local have = false
+		for _, oldFile in pairs(minFiles) do
+			if addFile == oldFile then
+				have = true
+				break
+			end
+		end
+		if not have then
+			table.insert(minFiles, addFile)
+		end
+	end
+end
+
+--
+
+if custom then
+	print("Using custom installation")
+	addFiles(custom)
+end
+
+-- Install blue api
+install(user, repo, "master", minFiles, dir)
+
+-- Init blue api
+shell.run(run)
+
+-- Download all files
+if not custom then
+	-- Load b_git
+	b_api.load("b_git")
+	-- Use b_git to install rest of api
+	b_git.install(user, repo, "master", dir, exclFiles)
+	-- Re-init (new init.lua available)
+	shell.run(run)
+end
---
+++ pastebin/ensure-blue-api.lua
@@ -0,0 +1,129 @@
+--[[
+Ensure BlueAPI
+Version 1.1
+
+Installs BlueAPI and components if needed
+Loads requested components automatically
+]]
+
+--[[
+Usage:
+
+pastebin run xQfeXVgj [apis..]
+
+Apis should be names of additional api's to load
+If given, they will be checked, and if not found, downloaded and loaded
+If blank, full api will be downloaded depending on the version
+
+Essential api's are loaded regardless, but can be included:
+b_api
+b_files
+b_git
+b_http
+b_io
+b_update
+
+]]
+
+local blueApiId = "yy7gqfBQ"
+local blueApiDir = "blue-api"
+local blueApiRun = fs.combine(blueApiDir, "init")
+
+local blueVersionUrl = "https://raw.githubusercontent.com/blueberrys/cc-blue-api/master/version"
+local blueVersionFile = fs.combine(blueApiDir, "version")
+
+-- For checking updates
+local minimalApis = {
+	"b_files",
+	"b_git",
+	"b_http",
+	"b_io",
+	"b_update",
+}
+
+--
+
+-- Check if minimal BlueAPI is available
+local function minimalCheck()
+	if not b_api then
+		if fs.exists(blueApiRun) then
+			shell.run(blueApiRun)
+		end
+		if not b_api then
+			return false
+		end
+	end
+
+	for _, api in pairs(minimalApis) do
+		if not b_api.load(api) then
+			return false
+		end
+	end
+
+	return true
+end
+
+-- Check if should get new version
+local function checkNewVersion()
+	if not minimalCheck() then
+		return true
+	else
+		return b_update.checkUpdate(blueVersionUrl, blueVersionFile)
+	end
+end
+
+-- Install api with params
+local function installBlueApi(params)
+	print("Installing blue-api (by Blueberrys)")
+	shell.run("pastebin run", blueApiId, params)
+end
+
+-- Installs BlueAPI and components if needed
+local function ensureBlueAPI(apis)
+	local function loadApis()
+		for _, api in pairs(apis) do
+			b_api.load(api)
+		end
+	end
+
+	-- Convert api's to list of params
+	local params = ""
+	for _, api in pairs(apis) do
+		params = params .. " apis/" .. api .. ".lua"
+	end
+	params = params:sub(2)
+
+	local should_install = false
+
+	if params=="" and checkNewVersion() then
+		-- No params, new version available
+		should_install = true
+	elseif not minimalCheck() then
+		-- Essentials are missing
+		should_install = true
+	else
+		-- Otherwise, check specifics
+		-- b_api and minimal api's must be available now
+
+		-- Check if all required api's exist
+		for _, api in pairs(apis) do
+			print("Checking ", api)
+			-- Isn't loaded, can't load, probably doesn't exist
+			if not _G[api] and not b_api.load(api) then
+				should_install = true
+				break
+			end
+		end
+	end -- essentials
+
+	if should_install then
+		-- Install
+		installBlueApi(params)
+	end
+
+	-- load all
+	loadApis()
+end
+
+local params = {...}
+ensureBlueAPI(params)
