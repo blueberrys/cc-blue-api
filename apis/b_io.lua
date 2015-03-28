d94267b6c1923207350ad5484f871823c08443eb
Blueberry <potatebleh+git@gmail.com>
2015-03-28 12:05:20 -0400

    b_io progress

--- apis/b_io.lua
+++ apis/b_io.lua
@@ -12,6 +12,21 @@ Defaults to print
 - setPrintFn(fn)
 Sets fn as the new print_fn
 
+- getPrintFn()
+Gets the current print_fn
+
+
+- progress(total, amt, msg)
+Send variables to the current progress_fn
+Default prints a progress bar and msg using print_fn
+
+- setProgressFn(fn)
+Sets fn as the new progress_fn
+
+- getProgressFn()
+Gets the current progress_fn
+
+
 - argToStr(...)
 Converts arguments to string
 
@@ -36,6 +51,32 @@ end
 
 function getPrintFn()
 	return print_fn
+end
+
+--
+
+local progress_fn = function(total, amt, msg)
+	local prg = ""
+	local prgAmt = 20/total*amt
+
+	for i=1, prgAmt, 1 do
+		prg = prg .. "="
+	end
+
+	print_fn("[" .. prg .. "]")
+	print_fn(msg)
+end
+
+function progress(total, amt, msg)
+	progress_fn(total, amt, msg)
+end
+
+function setProgressFn(fn)
+	progress_fn = fn
+end
+
+function getProgressFn()
+	return progress_fn
 end
 
 --
