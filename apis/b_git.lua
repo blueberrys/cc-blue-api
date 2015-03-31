08f75079ae4b4c31af94dac30788ba131a3b60b6
Blueberry <blueberrys@users.noreply.github.com>
2015-03-31 11:21:39 -0400

    install example

--- apis/b_git.lua
+++ apis/b_git.lua
@@ -20,7 +20,7 @@ repo		- GitHub repo name
 branch		- Github branch (default "master")
 path		- Local install path (default "/")
 exclude		- Table containing the relative path of folders/files to exclude
-eg: install("blueberrys", "cc-blue-api", "master", "blue-api", print, {"README.md"})
+eg: install("blueberrys", "cc-blue-api", "master", "blue-api", {"README.md"})
 
 ]]
 
